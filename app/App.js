7f6da94f35545f313b2f8e7205281426c9bceafc
Keith Lamond <keith.lamond@gmail.com>
2024-05-21 10:04:51 -0700

    moving navigation into App

--- app/App.js
+++ app/App.js
@@ -1,11 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./navigators/RootNavigator";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Onboarding from "../screens/Onboarding";
 
 // App.js is already setup by wrapping NavigationContainer around Root Navigator
 export default function App() {
   return (
     <NavigationContainer>
-      <RootNavigator />
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen
+          name="Onboarding"
+          component={Onboarding}
+          hidename="true"
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
